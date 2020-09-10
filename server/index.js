ec7a6bf795575889d38449f453d13cd6abddaa90
wainigwcan <285972812@qq.com>
2020-09-10 17:48:53 +0800

    集合

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ app.use(require("cors")());
 app.use(express.json());
 // 静态文件托管
 app.use('/uploads', express.static(__dirname + "/uploads"));
+app.use('/admin', express.static(__dirname + '/admin'))
+app.use('/', express.static(__dirname + '/web'))
 
 app.set('secret', 'i2u34y12oi3u4y8')
 
--- web/src/main.js
+++ web/src/main.js
@@ -7,7 +7,8 @@ import './assets/iconfont/iconfont.css'
 import axios from 'axios';
 
 Vue.prototype.$http = axios.create({
-  baseURL: "http://localhost:3000/web/api"
+  // baseURL: "http://localhost:3000/web/api",
+  baseURL: process.env.VUE_APP_API_URL || '/web/api'
 })
 
 import VueAwesomeSwiper from 'vue-awesome-swiper'
